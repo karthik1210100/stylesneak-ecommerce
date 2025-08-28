c6e91448e016f43c78243fb17b4548d56d7eddeb
karthik1210100 <138876533+karthik1210100@users.noreply.github.com>
2025-08-28 17:23:06 +0530

    deploy fail handled

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,10 +11,10 @@ const Footer = () => {
             <h3>StyleSneak</h3>
             <p>Premium footwear for every occasion. Step into style with our curated collection of sneakers.</p>
             <div className="social-icons">
-              <a href="#"><i className="fab fa-facebook"></i></a>
-              <a href="#"><i className="fab fa-instagram"></i></a>
-              <a href="#"><i className="fab fa-twitter"></i></a>
-              <a href="#"><i className="fab fa-pinterest"></i></a>
+              <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
+              <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+              <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter"></i></a>
+              <a href="https://pinterest.com" target="_blank" rel="noopener noreferrer"><i class="fab fa-pinterest-p"></i></a>
             </div>
           </div>
           
@@ -31,10 +31,10 @@ const Footer = () => {
           <div className="footer-section">
             <h3>Customer Service</h3>
             <ul>
-              <li><a href="#">Shipping Policy</a></li>
-              <li><a href="#">Returns & Exchanges</a></li>
-              <li><a href="#">Size Guide</a></li>
-              <li><a href="#">FAQs</a></li>
+              <li><a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Shipping Policy</a></li>
+              <li><a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Returns & Exchanges</a></li>
+              <li><a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Size Guide</a></li>
+              <li><a href="https://facebook.com" target="_blank" rel="noopener noreferrer">FAQs</a></li>
             </ul>
           </div>
           
